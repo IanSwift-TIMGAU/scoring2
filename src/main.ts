ee435c5ae322272c9ae21fd9db7f0345e2a45c46
Ian <ianswift@gmail.com>
2025-07-25 13:37:20 +1000

    fix build warnings

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 
 bootstrapApplication(App, {
   providers: [
